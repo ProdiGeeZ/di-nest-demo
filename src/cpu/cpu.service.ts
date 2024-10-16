27f424b539ffb2724e3c05c368ef084585591aa0
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-10-16 17:37:04 +0100

    import power module into disk module using DI

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -3,5 +3,11 @@ import { PowerService } from 'src/power/power.service';
 
 @Injectable()
 export class CpuService {
-    constructor(private powerService: PowerService){}
+    constructor(private powerService: PowerService) { }
+    
+    compute(a: number, b: number) {
+        console.log('Using 10MHz of power from Power service');
+        this.powerService.supplyPower(10);
+        return a + b;
+    }
 }
--- src/disk/disk.service.ts
+++ src/disk/disk.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+    constructor(private powerService: PowerService) { }
+    
+    getData() {
+        console.log('Using 20MHz of power from Power service');
+        this.powerService.supplyPower(20);
+        return 'Data!';
+    }
+}
