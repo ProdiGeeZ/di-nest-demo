7e0d52e0041e03821d005183499b45d474d3ea31
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-10-17 09:49:12 +0100

    import all other modules and services into computer module using DI

--- src/cpu/cpu.module.ts
+++ src/cpu/cpu.module.ts
@@ -4,6 +4,7 @@ import { PowerModule } from 'src/power/power.module';
 
 @Module({
   imports :[PowerModule],
-  providers: [CpuService]
+  providers: [CpuService], // this service is private unless exported
+  exports: [CpuService] //allows access from other modules
 })
 export class CpuModule {}
